706450f10cd75f495ea4a587f2c50114df410a15
maxim <maximmukhanov.job@gmail.com>
2022-07-09 19:56:07 +0000

    build: initialised and configured main packages

---
+++ commitlint.config.js
@@ -0,0 +1,50 @@
+// build: Изменения, которые влияют на систему сборки или внешние зависимости (примеры: gulp, broccoli, npm)
+// ci: Изменения в нашей CI(Continuous Integration) конфигурации (примеры: Travis, Circle, BrowserStack, SauceLabs)
+// docs: Изменения документов
+// feat: Новая фича
+// fix: Багфикс
+// perf: Изменение улучшающее работу приложения
+// refactor: Рефактор кода
+// style: Изменения, которые не меняют смысл кода (white-space, formatting, missing semi-colons, etc)
+// test: Тесты
+
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'body-leading-blank': [1, 'always'],
+      'body-max-line-length': [2, 'always', 100],
+      'footer-leading-blank': [1, 'always'],
+      'footer-max-line-length': [2, 'always', 100],
+      'header-max-length': [2, 'always', 100],
+      'scope-case': [2, 'always', 'lower-case'],
+      'subject-case': [
+        2,
+        'never',
+        ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+      ],
+      'subject-empty': [2, 'never'],
+      'subject-full-stop': [2, 'never', '.'],
+      'type-case': [2, 'always', 'lower-case'],
+      'type-empty': [2, 'never'],
+      'type-enum': [
+        2,
+        'always',
+        [
+          'build',
+          'chore',
+          'ci',
+          'docs',
+          'feat',
+          'fix',
+          'perf',
+          'refactor',
+          'revert',
+          'style',
+          'test',
+          'translation',
+          'security',
+          'changeset',
+        ],
+      ],
+    },
+  };
